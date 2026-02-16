a4a9f595b55d96b7a5a01ff27a10ebbbc2bc1adb
Kenneth Meyer <kmeyer2@utexas.edu>
2026-02-16 13:11:09 -0600

    Revert "Finished up Poisson tutorials. Starting work on NeoHookean"
    This reverts commit 04ff82dfcb7d2165d23b512d0806c6a6db391a84.

--- tutorials/Introductory/poisson/example.py
+++ tutorials/Introductory/poisson/example.py
@@ -1,6 +1,5 @@
 # Import some generally useful packages.
 import jax.numpy as np
-import jax
 import numpy as onp
 import time
 
@@ -14,35 +13,31 @@ class Poisson(Problem):
     # \int \nabla u \cdot \nabla v dx
     # the "\cdot \nabla v" is fixed, so only provide the \nabla u
     def get_tensor_map(self):
-
-        def tensor_map(u_grad, D):
-            return u_grad @ D
-
-        return tensor_map
+        return lambda u_grad: u_grad
     
     # Define the source term f
     # For the Poisson problem, using gaussian here
     def get_mass_map(self):
-        def mass_map(u, u_grad, x, D):
-            val = -3 * np.exp(-5 * ((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / (2 * 0.1 ** 2))
-            return np.array([val])
+        def mass_map(u, u_grad, x):
+            val = -np.array([15.*np.exp(-(np.power(x[0] - 0.5, 2) + np.power(x[1] - 0.5, 2)) / 0.02)])
+            return val
         return mass_map
 
     # Define potential surface kernels
     # Just sinusoidal here
     def get_surface_maps(self):
         def surface_map1(u, u_grad, x, a):
-            return -np.sin(2 * a*x[0]*np.pi).reshape(1,)
+            return -np.sin(a*x[0]).reshape(1,)
 
         def surface_map2(u, u_grad, x, a):
-            return np.sin(2 * a*x[0]*np.pi).reshape(1,)
+            return np.sin(a*x[0]).reshape(1,)
 
         return {"u": {"bottom": surface_map1, "top": surface_map2}}
     
 # Create the mesh and FE field
 Lx, Ly = 1., 1.
-mesh = rectangle_mesh(Nx=50, Ny=50, Lx=Lx, Ly=Ly, ele_type="quad8")
-fe = FiniteElement(mesh, vec=1, dim=2, ele_type="quad8", gauss_order=2)
+mesh = rectangle_mesh(Nx=32, Ny=32, Lx=Lx, Ly=Ly)
+fe = FiniteElement(mesh, vec=1, dim=2, ele_type="quad", gauss_order=1)
 
 # Define boundary locations.
 def left(point):
@@ -68,66 +63,64 @@ dirichlet_bc_info = {"u": [bc_left, bc_right]}
 location_fns = {"u": {"bottom": bottom, "top": top}}
 
 problem = Poisson({"u": fe}, dirichlet_bc_info=dirichlet_bc_info, location_fns=location_fns)
+problem.set_internal_vars_surfaces({"u": {"bottom": {"a": np.array([5.])}, "top": {"a": np.array([5.])}}})
 
 # Create instance of Newton_Solver
 solver = Newton_Solver(problem, np.zeros((len(mesh.points), 1)))
 
-### Creates variables to solve over
-num_frames = 51
-a_values = np.linspace(-1, 1, num_frames, endpoint=True)
-a_values = np.hstack([a_values, a_values[::-1]])
-
-@jax.vmap
-def D_vals(t):
-    return np.array([[1., .5*t], [.5*t, 1.]])
-
-D_values = D_vals(a_values)
-
 # Solve the problem
-toc_jit = time.time()
-problem.set_internal_vars({"u": {"D": D_values[0]}})
-problem.set_internal_vars_surfaces({"u": {"bottom": {"a": np.array([a_values[0]])}, "top": {"a": np.array([a_values[0]])}}})
-sol0, info = solver.solve(atol=1e-6)
+# sol, info = solver.solve(atol=1e-6)
+# assert info[0]
+
+### Solving for multiple frames
+# Start from a = -5, go to 5 and back to -5
+num_frames = 100
+a_values = np.linspace(-5., 5., num_frames//2, endpoint=True)
+a_values = np.concatenate((a_values, a_values[::-1]))
+
+n = -1
+problem.set_internal_vars_surfaces({"u": {"bottom": {"a": np.array([a_values[n]])}, "top": {"a": np.array([a_values[n]])}}})
+tic = time.time()
+sol0, info = solver.solve()
 assert info[0]
-tic_jit = time.time()
+toc0 = time.time() - tic
 
 sols = []
-toc = time.time()
-for n in range(len(a_values)):
-    problem.set_internal_vars({"u": {"D": D_values[n]}})
+tic = time.time()
+for n in range(num_frames):
     problem.set_internal_vars_surfaces({"u": {"bottom": {"a": np.array([a_values[n]])}, "top": {"a": np.array([a_values[n]])}}})
     sol, info = solver.solve()
     assert info[0]
     sols.append(onp.array(sol))
-tic = time.time()
+toc = time.time()
 
-print("Initial solve time:", tic_jit - toc_jit)
-print("Total solve time for all frames:", tic - toc)
-print("Average time per frame:", (tic - toc)/(len(a_values)))
+print("Initial solve time:", toc0)
+print("Total solve time for all frames:", toc - tic)
+print("Average time per frame:", (toc - tic)/num_frames)
 
-if plotting := True:
+if plotting := False:
     import pyvista as pv
 
     pl = pv.Plotter(off_screen=True)
     mesh.point_data["sol"] = sol0.reshape(-1,)
     warped = mesh.warp_by_scalar("sol", factor=1.)
-    pl.add_mesh(warped, cmap="inferno", show_edges=True)
-    pl.screenshot("../../figures/Introductory/poisson/poisson_initial.png")
+    pl.add_mesh(warped)
+    pl.screenshot("poisson_initial.png")
     pl.close()
 
     pl = pv.Plotter(off_screen=True)
     mesh.point_data["sol"] = sols[0].reshape(-1,)
     warped = mesh.warp_by_scalar("sol", factor=1.)
 
-    pl.add_mesh(warped, cmap="inferno", show_edges=True)
-    pl.open_gif("../../figures/Introductory/poisson/poisson_movie.gif")
+    pl.add_mesh(warped)
+    pl.open_gif("poisson_movie.gif")
 
     for i, s in enumerate(sols):
         pl.clear()
         mesh.point_data["sol"] = s.reshape(-1,)
         warped = mesh.warp_by_scalar("sol", factor=1.)
         pl.add_title(f"a = {a_values[i]:.2f}")
-        pl.add_mesh(warped, reset_camera=False, cmap="inferno", show_edges=True)
+        pl.add_mesh(warped, reset_camera=False, cmap="reds")
         pl.write_frame()
     pl.close()
 
