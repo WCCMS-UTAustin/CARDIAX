9419a291ee4b332e64c234f78290cc85c9c70274
Gabriel Peery <67531541+g-peery@users.noreply.github.com>
2026-04-07 23:23:09 +0000

    Fix syntax error in Python<3.12
    Nested quotes aren't allowed before 3.12

--- src/cardiax/input_file/input_file.py
+++ src/cardiax/input_file/input_file.py
@@ -227,7 +227,7 @@ class ProblemManager():
 
         # Check if PDE is in predefined PDEs
         elif config.pde_info["pde_class"] in pde_dirs:
-            pde_path = list(base_path.glob(f"**/{config.pde_info["pde_class"]}"))[0]
+            pde_path = list(base_path.glob(f"**/{config.pde_info['pde_class']}"))[0]
             pdes = {p.name[:-3] for p in pde_path.glob("*.py")}
             assert config.pde_info["pde_type"] in pdes, f"PDE type not supported. Please choose between {pdes}"
             mm = f"{pde_path}/{config.pde_info["pde_type"]}"
